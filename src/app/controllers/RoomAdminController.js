f420ea86e2578dc3b1b4ef140df3d1d2e93552c4
Nguyenle23 <beenbin2308@gmail.com>
2022-04-23 13:39:12 +0700

    Update RoomAdminController.js

--- src/app/controllers/RoomAdminController.js
+++ src/app/controllers/RoomAdminController.js
@@ -4,48 +4,49 @@ const Customer = require('../models/Customer');
 const { multipleToObject } = require('../../config/utility/mongoose');
 const { mongooseToObject } = require('../../config/utility/mongoose');
 
-//GET
-const showRoom =  (req, res, next) => {
+//[GET] /admin/room/
+const showRoom = (req, res, next) => {
     Room.find()
         .then(rooms => {
-            res.render('TabRoomsAdmin/roomsAdmin', { layout: 'mainAdmin.hbs', 
-            rooms: multipleToObject(rooms) });
+            res.render('TabRoomsAdmin/roomsAdmin', {
+                layout: 'mainAdmin.hbs',
+                rooms: multipleToObject(rooms)
+            });
         })
         .catch(next);
 }
 
-
-const quickSearchRoom = async (req, res, next) => {
+//[GET] /admin/room/query/:attribute
+const quickSearchRoom = async(req, res, next) => {
     // lấy giá trị bấm bên categories
     console.log(req.params.attribute);
     var room;
-    if(req.params.attribute == 'controng') {
-        room = await Room.find({r_status: 'còn trống'});
-    }
-    else if(req.params.attribute == 'dangsudung'){
-        room = await Room.find({r_status: 'đang sử dụng'});
+    if (req.params.attribute == 'controng') {
+        room = await Room.find({ r_status: 'còn trống' });
+    } else if (req.params.attribute == 'dangsudung') {
+        room = await Room.find({ r_status: 'đang sử dụng' });
     }
     res.render('TabRoomsAdmin/roomsAdmin', { layout: 'mainAdmin.hbs', rooms: multipleToObject(room) });
 }
 
-const add =  (req, res, next) => {
+//[GET] /admin/room/edit
+const add = (req, res, next) => {
     Room.find()
         .then(rooms => {
-            res.render('TabRoomsAdmin/addRoomAdmin', { layout: 'mainAdmin.hbs', 
-            rooms: multipleToObject(rooms) });
+            res.render('TabRoomsAdmin/addRoomAdmin', {
+                layout: 'mainAdmin.hbs',
+                rooms: multipleToObject(rooms)
+            });
         })
         .catch(next);
 }
 
-const store =  (req, res, next) => {
+//[GET] /admin/add/store
+const store = (req, res, next) => {
     const room = new Room(req.body);
-    room.save() 
-        .then( res.redirect('/admin/room'))
-        .catch(error =>{
-        })     
+    room.save()
+        .then(res.redirect('/admin/room'))
+        .catch(error => {})
 }
 
-
-
 module.exports = { showRoom, quickSearchRoom, add, store };
-
