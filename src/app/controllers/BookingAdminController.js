e0087d3a91e6aa5820e99eae1b58e71e78bf22d8
KhanhLeee <93957393+KhanhLeee@users.noreply.github.com>
2022-04-25 20:38:27 +0700

    update admin UI

--- src/app/controllers/BookingAdminController.js
+++ src/app/controllers/BookingAdminController.js
@@ -43,7 +43,8 @@ const quickSearchBooking = async (req, res, next) => {
         
         customers = await Customer.find({c_status: 'Đã xác nhận'});
     }
-    res.render('TabBookingAdmin/bookingAdmin', { layout: 'mainAdmin.hbs', customers: multipleToObject(customers) });
+    console.log(customers)
+    // res.render('TabBookingAdmin/bookingAdmin', { layout: 'mainAdmin.hbs', customers: multipleToObject(customers) });
 }
 
 module.exports = {  showBooking, confirm, updateStatus, quickSearchBooking};
