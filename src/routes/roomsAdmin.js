1245c906a0bbdba2834abd9e4ca4e9b9e69e319d
= <nguyenthituyetngan5142@gmail.com>
2022-04-26 14:16:40 +0700

    ngan commit edit room admin

--- src/routes/roomsAdmin.js
+++ src/routes/roomsAdmin.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const roomAdminController = require('../app/controllers/RoomAdminController');
 
 router.post('/room/add/store', roomAdminController.store);
+router.put('/room/:id', roomAdminController.update);
+router.get('/room/:id/edit', roomAdminController.edit);
 router.get('/room/add', roomAdminController.add);
 router.get ('/room/query/:attribute', roomAdminController.quickSearchRoom);
 router.get('/room', roomAdminController.showRoom);
