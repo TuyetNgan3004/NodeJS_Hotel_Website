1a63fee0b00f841d743c6beb4cecb0d892f57c14
Ngo Quoc Tu <19dh110772@st.huflit.edu.vn>
2022-05-15 20:45:40 -0700

    Update Search admin

--- src/public/js/filter.js
+++ src/public/js/filter.js
@@ -7,3 +7,24 @@ $(document).ready(function(){
     });
   });
 });
+
+// Filter rooms by searchbar 
+const searchingWord = document.querySelector('#myInput').addEventListener("keyup", function () {
+  const keyword = document.querySelector('#myInput');
+  const value = keyword.value.toLowerCase();
+
+  const rooms = document.querySelectorAll('.js-myroom').forEach(room => {
+     var items = room.querySelector('*').innerText;
+     var itemsRemoveComma = items.replaceAll(',', '');
+     if (value == '') {
+        room.classList.remove("room-filter-none")
+     }
+     else if (itemsRemoveComma.toLowerCase().indexOf(value) > -1) {
+        room.classList.remove("room-filter-none")
+     }
+
+     else {
+        room.classList.add("room-filter-none")
+     }
+  });
+})
