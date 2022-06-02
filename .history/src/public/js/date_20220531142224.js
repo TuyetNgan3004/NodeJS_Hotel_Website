d01da0a29afd63f9bdbefb0c7e1c9b5d66d1732f
KhanhLeee <93957393+KhanhLeee@users.noreply.github.com>
2022-06-02 13:34:55 +0700

    update 2/6 complete bill

---
+++ .history/src/public/js/date_20220531142224.js
@@ -0,0 +1,19 @@
+var checkoutDate = document.querySelector('[data-toggle="endDate"]').flatpickr(
+   {
+      altInput: true,
+      altFormat: "d-m-Y",
+      minDate: new Date().fp_incr(0),
+   }
+ );
+
+var checkinDate = document.querySelector('[data-toggle="startDate"]').flatpickr(
+   {
+      altInput: true,
+      altFormat: "d-m-Y",
+      minDate: new Date().fp_incr(0),
+      onChange: function(dateObj, dateStr) {  
+         checkoutDate.set('minDate', new Date(dateStr).fp_incr(1));
+         // flatpickr(checkoutDate, {dateFormat: "d-m-Y", minDate: new Date(dateStr).fp_incr(1)});
+     }
+   }
+ );
