2ecf56da7940668d079680f56505eadad4d6f0d0
= <nguyenthituyetngan5142@gmail.com>
2022-06-10 22:02:59 +0700

    Ngan thong ke theo thang con loi giao dien

--- src/app/controllers/StatisticAdminController.js
+++ src/app/controllers/StatisticAdminController.js
@@ -3,14 +3,68 @@ const Bill = require('../models/Bill');
 
 const { multipleToObject } = require('../../config/utility/mongoose');
 const { mongooseToObject } = require('../../config/utility/mongoose');
+const { query } = require('express');
 
 const statisticDay = async (req, res, next) => {
    res.render('TabStatisticAdmin/statisticAdmin', { layout: 'mainAdmin.hbs' });
 }
 
 const filterMonth = async (req, res, next) => {
+
    const inputMonth = req.body.month;
    console.log(inputMonth)
+
+   const monthBill = await Bill.aggregate([
+      { $match: { "b_status": "Đã thanh toán" } },
+      {
+         $group: {
+            _id: "$createdAt",
+            price:
+            {
+               $sum: {
+                  $convert: {
+                     input: {
+                        $reduce: {
+                           input: {
+                              $split: ['$b_total', ',']
+                           },
+                           initialValue: '',
+                           in: {
+                              $concat: ['$$value', '$$this']
+                           }
+                        }
+                     },
+                     to: 'int',
+                     onError: 0
+                  }
+               }
+            },
+         }
+      }
+   ])
+   // console.log(monthBill)
+
+   const objMonth = Object.assign({}, monthBill);
+   var countOrder = 0;
+   var total = 0;
+   for (let i in objMonth) {
+      if (objMonth[i]._id.getMonth() == (inputMonth - 1)) {
+         countOrder++;
+         total += parseFloat(objMonth[i].price);
+         console.log(objMonth[i]);
+      }
+   }
+   total = Intl.NumberFormat().format(total);
+   res.render('TabStatisticAdmin/statisticAdmin', {
+      layout: 'mainAdmin.hbs',
+      objMonth,
+      total,
+      countOrder
+   });
+ 
+
+   console.log(countOrder);
+
 }
 
 module.exports = { statisticDay, filterMonth };
--- src/public/js/statistic.js
+++ src/public/js/statistic.js
@@ -4,11 +4,6 @@ var statisticDay = document.querySelector(".statistic-day");
 var btnFilterMonth = document.querySelector(".filter-month");
 
 btnFilterMonth.addEventListener('click', function () {
-
-    if (statisticMonth.style.display === 'none') {
-        statisticDay.style.display = 'none';
-        statisticMonth.style.display = 'block';
-    } else {
-        statisticMonth.style.display = 'none';
-    }
+    statisticDay.style.display = 'none';
+    statisticMonth.style.display = 'block';
 });
